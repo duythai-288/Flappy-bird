17980530ce6d20be768d7128ca49d5cb60e8705b
duythai-288 <thaipd@monsterstudio.io>
2023-04-17 19:54:04 +0700

    flappy bird

---
+++ Flappy Birth 2820/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+    public GameObject getReady;
+
+    private int score;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        Pause();
+    }
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+        getReady.SetActive(false);
+
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+    public void GameOver()
+    {
+        playButton.SetActive(true);
+        gameOver.SetActive(true);
+        getReady.SetActive(true);
+
+        Pause();
+    }
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+ }
